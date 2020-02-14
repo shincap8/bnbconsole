0d8417e63542ffcc32034885beb2af880e8e9734
shincap8 <shincap@gmail.com>
2020-02-14 19:07:34 +0000

    Task 5 id wrong when upload

--- models/base_model.py
+++ models/base_model.py
@@ -17,14 +17,14 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            storage.new(self.to_dict())
 
     def save(self):
-        storage.save()
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
-        keys = self.__dict__
+        keys = self.__dict__.copy()
         keys['__class__'] = self.__class__.__name__
         keys["created_at"] = keys["created_at"].isoformat()
         keys["updated_at"] = keys["updated_at"].isoformat()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -16,11 +16,10 @@ class FileStorage:
         return self.__objects
  
     def new(self, obj):
-        key = obj.__class__.__name__ + "." + obj.id
+        key = obj["__class__"] + "." + obj["id"]
         self.__objects[key] = obj
 
     def save(self):
-        self.models.to_dict()
         with open(self.__file_path, mode="w") as f:
             text = json.dumps(self.__objects)
             f.write(text)
@@ -29,5 +28,5 @@ class FileStorage:
         if path.isfile(self.__file_path):
             with open(self.__file_path, mode="r") as f:
                 text = f.read()
-                self.__objects = json.loads(text)
-            
+                if len(text) > 0:
+                    self.__objects = json.loads(text)
