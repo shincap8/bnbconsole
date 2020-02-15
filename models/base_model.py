996d5b024eda7900602a1ae928f3b8f9ced2031e
jormao <jormao@gmail.com>
2020-02-15 07:49:33 -0500

    advance

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,7 @@
 """Comment"""
 from datetime import datetime, date
 import uuid
-from models.__init__ import storage
+import models
 
 
 class BaseModel:
@@ -11,24 +11,24 @@ class BaseModel:
             for k, v in kwargs.items():
                 if k == "updated_at" or k == "created_at":
                     setattr(self, k, datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f"))
-                elif k is not "__class__":
+                elif k != "__class__":
                     setattr(self, k, v)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            models.storage.new(self)
 
     def save(self):
-        storage.save()
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
-        keys = self.__dict__
+        keys = self.__dict__.copy()
         keys['__class__'] = self.__class__.__name__
         keys["created_at"] = keys["created_at"].isoformat()
         keys["updated_at"] = keys["updated_at"].isoformat()
         return keys
 
     def __str__(self):
-        return ("[BaseModel] (" + self.id + ") " + str(self.__dict__))
+        return ("[" + self.__class__.__name__ + "] (" + self.id + ") " + str(self.__dict__))
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,29 +5,30 @@
 import json
 import os.path as path
 import models
+from models.base_model import BaseModel
 
 
 class FileStorage:
-    def __init__(self):
-        self.__file_path = "file.json"
-        self.__objects = {}
+    __file_path = "file.json"
+    __objects = {}
 
     def all(self):
         return self.__objects
- 
+
     def new(self, obj):
         key = obj.__class__.__name__ + "." + obj.id
         self.__objects[key] = obj
 
     def save(self):
-        self.models.to_dict()
-        with open(self.__file_path, mode="w") as f:
-            text = json.dumps(self.__objects)
-            f.write(text)
+        new_dictionary = {}
+        with open(self.__file_path, mode="w", encoding='UTF-8') as f:
+            for k, v in self.__objects.items():
+                new_dictionary[k] = v.to_dict()
+            json.dump(new_dictionary, f)
 
     def reload(self):
         if path.isfile(self.__file_path):
-            with open(self.__file_path, mode="r") as f:
-                text = f.read()
-                self.__objects = json.loads(text)
-            
+            with open(self.__file_path, mode="r", encoding='UTF-8') as f:
+                for k, v in (json.load(f)).items():
+                    v = eval(v["__class__"])(**v)
+                    self.__objects[k] = v
